ecbae49c8e3393790d5e3cb3e2ba7117d2577931
Grant - Veeps Hosting <grant@veepshosting.com>
2023-10-31 15:14:13 +1100

    .

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,58 @@
 class site24x7 (
-  String $device_key  = '',
-  String $agent_proxy = '',
-) inherits site24x7::params{
-  include site24x7::linuxagent
+  String                  $agent_proxy    = '',
+  String                  $device_key     = undef,
+  Boolean                 $service_enable = true,
+  Stdlib::Ensure::Service $service_ensure = 'running',
+  ) {
+
+  ## Determine the most appropriate architecture for the installer file
+  if $facts[os][architecture] == 'amd64' or 'x86_64' {
+    $install_file="Site24x7_Linux_64bit.install"
+  } else {
+    $install_file="Site24x7_Linux_32bit.install"
+  }
+
+  ## Create the installer directory
+  file { '/opt/.site24x7agent':
+    ensure => directory,
+    mode   => '0755',
+  }
+
+  ## Download the installer file and ensure it's executable
+  exec { 'Download Site24x7 Installer':
+    command => "curl https://staticdownloads.site24x7.com//server//$install_file > /opt/.site24x7agent/Linux_Agent.install",
+    creates => '/opt/.site24x7agent/Linux_Agent.install',
+    notify  => Exec['Install Site24x7 Agent'],
+    path    => $::path,
+    require => File['/opt/.site24x7agent'],
+    unless  => 'test -f /opt/.site24x7agent/Linux_Agent.install',
+  }
+  file { '/opt/.site24x7agent/Linux_Agent.install':
+    mode    => '0755',
+    require => Exec['Download Site24x7 Installer'],
+  }
+
+  ## Determine if a proxy is being used
+  if $agent_proxy != '' {
+    $command = "/opt/.site24x7agent/./Linux_Agent.install -i -key=${device_key} -installer=puppet -proxy=${agent_proxy}"
+  } else {
+    $command = "/opt/.site24x7agent/./Linux_Agent.install -i -key=${device_key} -installer=puppet"
+  }
+
+  ## Install agent from installer file
+  exec { 'Install Site24x7 Agent':
+    command     => $command,
+    creates     => '/opt/site24x7/monagent/bin/monagent',
+    path        => [$::path,'/opt/.site24x7agent'],
+    require     => File['/opt/.site24x7agent/Linux_Agent.install'],
+    refreshonly => true,
+  }
+
+  ## Manage system service
+  service { 'site24x7monagent':
+    enable  => $service_enable,
+    ensure  => $service_ensure,
+    require => Exec['Install Site24x7 Agent'],
+  }
+
 }
--- manifests/installation_handler.pp
+++
@@ -1,9 +0,0 @@
-class site24x7::installation_handler {
-	exec { 'Install Linux Agent With Proxy':
-	  command => "${site24x7::linuxagent::command}",
-	  creates => '/opt/site24x7/monagent/bin/monagent',
-	  path    => '/bin:/sbin:/usr/bin:/usr/sbin:/opt/.site24x7agent',
-		require => File['/opt/.site24x7agent/Linux_Agent.install'],
-	  unless  => "/usr/bin/test -d /opt/site24x7/monagent",
-	}
-}
